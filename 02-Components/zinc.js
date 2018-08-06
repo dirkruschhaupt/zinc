ffb95af596a9d305b16b0f410eff3cd766fa3a6b
Dirk Ruschhaupt <dirkruschhaupt@Dirks-MacBook-Pro.local>
2018-08-06 13:45:12 -0500

    finished

--- 02-Components/zinc.js
+++ 02-Components/zinc.js
@@ -6,9 +6,15 @@ const Zinc = {};
 
 (() => {
 
-  function renderComponent(element, content, data) {
+  function hilite() {
+    this.firstElementChild.classList.toggle('hilight');
+    console.log(hilight);
+  };
+
+  function renderComponent(element, content, data, controller) {
     let regex = /{{\s*([\w.]+)\s*}}/g;
     let elements = Array.from(document.getElementsByTagName(element));
+
     fetch(`${content}.html`)
       .then(content => content.text())
       .then((content) => {
@@ -17,6 +23,7 @@ const Zinc = {};
             let templateValueArr = templateValue.split('.');
             return templateValueArr.reduce((acc, curr) => acc[curr], data)
           })
+          element.addEventListener('click', controller);
           element.insertAdjacentHTML('beforeend', HTML);
         })
       })
@@ -30,35 +37,37 @@ const Zinc = {};
         renderComponent(
           components[component].elementName,
           components[component].templateFile,
-          components[component].dataObject);
+          components[component].dataObject,
+          components[component].controller)
       }
     }
 
-    Zinc.registerComponent = function(elementName, templateFile, dataObject) {
+    Zinc.registerComponent = function(elementName, templateFile, dataObject, controller) {
         if (!Zinc.components) {
           Zinc.components = {};
         }
         Zinc.components[elementName] = {
           elementName,
           templateFile,
-          dataObject
-        };
+          dataObject,
+          controller
+        }
     }
 
     function init() {
-      Zinc.registerComponent('user-item', 'user', Zinc.userData);
-      console.log(Zinc.components);
-      renderComponents(Zinc.components);
+      //removes Jack form the list
+      // Zinc.registerComponent('user-item', 'user', Zinc.userData, controller);
+      // renderComponents(Zinc.components);
 
-      fetch('https://randomuser.me/api/?results=5')
+      fetch('https://randomuser.me/api/?results=1')
       .then(res => res.json())
       .then(data => {
         data.results.forEach(user => {
-          Zinc.registerComponent('user-item', 'user', user);
+          Zinc.registerComponent('user-item', 'user', user, hilite);
           renderComponents(Zinc.components);
         })
-    })
-}
+      })
+    }
     document.addEventListener('DOMContentLoaded', init);
 
 })();
